97f81d7eb043dd6f6244991ea03ac341454208ce
Zanie Blue <contact@zanie.dev>
2024-10-16 16:24:17 -0500

    Add `--group` and `--only-group` to `uv sync` and includes all groups in `uv lock`

--- crates/uv/tests/it/edit.rs
+++ crates/uv/tests/it/edit.rs
@@ -4081,8 +4081,12 @@ fn add_group() -> Result<()> {
     ----- stdout -----
 
     ----- stderr -----
-    Resolved 1 package in [TIME]
-    Audited in [TIME]
+    Resolved 4 packages in [TIME]
+    Prepared 3 packages in [TIME]
+    Installed 3 packages in [TIME]
+     + anyio==3.7.0
+     + idna==3.6
+     + sniffio==1.3.1
     "###);
 
     let pyproject_toml = context.read("pyproject.toml");
@@ -4112,8 +4116,13 @@ fn add_group() -> Result<()> {
     ----- stdout -----
 
     ----- stderr -----
-    Resolved 1 package in [TIME]
-    Audited in [TIME]
+    Resolved 10 packages in [TIME]
+    Prepared 4 packages in [TIME]
+    Installed 4 packages in [TIME]
+     + attrs==23.2.0
+     + outcome==1.3.0.post0
+     + sortedcontainers==2.4.0
+     + trio==0.25.0
     "###);
 
     let pyproject_toml = context.read("pyproject.toml");
@@ -4132,7 +4141,7 @@ fn add_group() -> Result<()> {
         [dependency-groups]
         test = [
             "anyio==3.7.0",
-            "trio",
+            "trio>=0.25.0",
         ]
         "###
         );
@@ -4144,8 +4153,8 @@ fn add_group() -> Result<()> {
     ----- stdout -----
 
     ----- stderr -----
-    Resolved 1 package in [TIME]
-    Audited in [TIME]
+    Resolved 10 packages in [TIME]
+    Audited 3 packages in [TIME]
     "###);
 
     let pyproject_toml = context.read("pyproject.toml");
@@ -4164,7 +4173,7 @@ fn add_group() -> Result<()> {
         [dependency-groups]
         test = [
             "anyio==3.7.0",
-            "trio",
+            "trio>=0.25.0",
         ]
         second = [
             "anyio==3.7.0",
