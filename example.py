aec0aa1c9df79526d9722d9d8f6074d1393336cb
Zanie Blue <contact@zanie.dev>
2026-05-04 23:54:11 -0700

    WIP

---
+++ example.py
@@ -0,0 +1,13 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = []
+# ///
+
+
+def main() -> None:
+    print("Hello from example.py!")
+
+
+if __name__ == "__main__":
+    main()
+import sys; print(sys.executable)
